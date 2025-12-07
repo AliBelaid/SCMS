8ce4b3d411745530918f6b2407036a0ed0683074
AliBelaid <alimofthabelaid@gmail.com>
2025-12-07 02:55:16 +0200

    fxid_update

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d709850639b2fd59e7eddd5a53c6c5f4550ffc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ea7dc871dea14bab47c6e75cb0322e43afe93e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
